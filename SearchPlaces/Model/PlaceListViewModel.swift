355d5ad2a2beb5b9b3b2f04a1e3929a1c94b7059
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2023-11-08 19:43:49 +1100

    Model files refactored

--- SearchPlaces/Model/PlaceListViewModel.swift
+++ SearchPlaces/Model/PlaceListViewModel.swift
@@ -1,12 +1,6 @@
-//
-//  PlaceListViewModel.swift
-//  SearchAPlace
-//
-//  Created by Anup Kuriakose on 4/11/2023.
-//
-
 import Foundation
 
+// Structure to represent a place with various attributes.
 struct Place {
     var iconPrefix: String?
     var iconSuffix: String?
@@ -20,35 +14,44 @@ struct Place {
     var region: String?
     var name: String?
 
+    // Initializer that creates a `Place` instance from a dictionary, typically from JSON parsing.
     init(from dict: [String: Any]) {
+        // Parses the icon information from the first category if available.
         if let categories = dict["categories"] as? [[String: Any]], let firstCategory = categories.first {
-            print("Categories:\(categories)")
             if let iconDict = firstCategory["icon"] as? [String: Any] {
-                        self.iconPrefix = iconDict["prefix"] as? String
-                        self.iconSuffix = iconDict["suffix"] as? String
+                self.iconPrefix = iconDict["prefix"] as? String
+                self.iconSuffix = iconDict["suffix"] as? String
             }
             
+            // Parses the category name from the first category.
             self.categoryName = firstCategory["name"] as? String
         }
+        
+        // Parses additional place attributes from the dictionary.
         self.distance = dict["distance"] as? Double
         self.fsqId = dict["fsq_id"] as? String
         
+        // Parses the latitude and longitude information if available.
         if let geocodes = dict["geocodes"] as? [String: Any], let main = geocodes["main"] as? [String: Any] {
             self.latitude = main["latitude"] as? Double
             self.longitude = main["longitude"] as? Double
         }
         
+        // Parses the location information such as address, postcode, and region.
         if let location = dict["location"] as? [String: Any] {
             self.address = location["address"] as? String
             self.postcode = location["postcode"] as? String
             self.region = location["region"] as? String
         }
         
+        // Parses the name of the place.
         self.name = dict["name"] as? String
     }
 }
 
+// Extension of `Place` to provide a convenience initializer.
 extension Place {
+    // Convenience initializer to create a `Place` instance directly from individual properties.
     init(iconPrefix: String, iconSuffix: String, categoryName: String, distance: Double, fsqId: String, latitude: Double, longitude: Double, address: String, postcode: String, region: String, name: String) {
         self.iconPrefix = iconPrefix
         self.iconSuffix = iconSuffix
@@ -63,3 +66,4 @@ extension Place {
         self.name = name
     }
 }
+
--- SearchPlaces/Model/PlacePhotoModel.swift
+++ SearchPlaces/Model/PlacePhotoModel.swift
@@ -1,10 +1,16 @@
 import Foundation
 
+// Defines a structure to represent a photo with its URL components.
 struct PlacePhoto {
+    // Constants for the prefix and suffix parts of the photo URL.
     let prefix: String
     let suffix: String
     
+    // Failable initializer that creates a `PlacePhoto` instance from a dictionary.
+    // If either `prefix` or `suffix` is not found, the initializer fails by returning nil.
     init?(dictionary: [String: Any]) {
+        // Checks for 'prefix' and 'suffix' keys in the dictionary.
+        // If they are not found or not of type String, initialization fails.
         guard let prefix = dictionary["prefix"] as? String,
               let suffix = dictionary["suffix"] as? String else {
             return nil
@@ -13,11 +19,14 @@ struct PlacePhoto {
         self.suffix = suffix
     }
     
+    // Non-failable initializer that creates a `PlacePhoto` instance with given prefix and suffix strings.
     init(prefix: String, suffix: String) {
         self.prefix = prefix
         self.suffix = suffix
     }
     
+    // Computed property that constructs a full image URL from the prefix and suffix.
+    // '200' is presumably a size or other URL component added between the prefix and suffix.
     var imageURL: URL? {
         return URL(string: prefix + "200" + suffix)
     }
